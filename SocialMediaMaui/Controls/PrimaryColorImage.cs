dbfe25b5fcd9f5a0c0172272b2660f946665bf46
Developer <Developer@DEV>
2025-01-15 15:40:48 +0300

    # 8 of 43 - Build Fullstack Social Media App .NET MAUI XAML + Asp.Net Core Minimal Api + SignalR

---
+++ SocialMediaMaui/Controls/PrimaryColorImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.Behaviors;
+
+namespace SocialMediaMaui.Controls
+{
+    public class PrimaryColorImage : Image
+    {
+        public PrimaryColorImage()
+        {
+            if (App.Current.Resources.TryGetValue("Primary", out object color) && color is Color primaryColor)
+            {
+                var tintColorBehavior = Behaviors.FirstOrDefault(b => b is IconTintColorBehavior);
+                if (tintColorBehavior == null)
+                {
+                    tintColorBehavior = new IconTintColorBehavior
+                    {
+                        TintColor = primaryColor
+                    };
+                    Behaviors.Add(tintColorBehavior);
+                }
+            }
+        }
+    }
+}
--- SocialMediaMaui/Controls/WhiteImage.cs
+++ SocialMediaMaui/Controls/WhiteImage.cs
@@ -22,4 +22,6 @@ namespace SocialMediaMaui.Controls
             }
         }
     }
+
+  
 }
